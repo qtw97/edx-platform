4db2f09d8e820c1c24fbdeeb9c039fb424be3074
qtw97 <qiutianw@gmail.com>
2023-10-29 16:48:40 -0400

    enable course discovery

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -657,7 +657,7 @@ FEATURES = {
     # .. toggle_target_removal_date: None
     # .. toggle_warning: The COURSE_DISCOVERY_MEANINGS setting should be properly defined.
     # .. toggle_tickets: https://github.com/openedx/edx-platform/pull/7845
-    'ENABLE_COURSE_DISCOVERY': False,
+    'ENABLE_COURSE_DISCOVERY': True,
 
     # .. toggle_name: FEATURES['ENABLE_COURSE_FILENAME_CCX_SUFFIX']
     # .. toggle_implementation: DjangoSetting
